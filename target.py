3861b2e040a12ba0dee37bf6531d88a0a405df95
zknutson <zach.knutson@cfa.harvard.edu>
2026-03-03 16:26:45 -0500

    refactored tns_table code to instead return a list of Target objects

--- target.py
+++ target.py
@@ -1,27 +1,23 @@
 from dataclasses import dataclass
+from turtle import pd
 import alerce
 from pandas import DataFrame
 from astroquery.ipac.irsa.irsa_dust import IrsaDust
 
 @dataclass
 class Target:
-    oid: str
-    redshift: float
-    fid: list[int]
-    mjd: list[float]
-    mag: list[float]
-
-    @classmethod
-    def from_oid_rsft(cls, oid, redshift):
+    def __init__(self, oid, redshift, ra, dec):
+        self.oid: str = oid
+        self.redshift: float = redshift
+        self.coordinates: tuple[float, float] = (ra, dec)
         observations = alerce.query_lightcurve(
             oid=oid,
             format="pandas"
         )
         detections = DataFrame(observations["detections"][0])
-        fid = detections["fid"]
-        mjd = detections["mjd"]
-        mag = detections["magpsf"]
-        return cls(oid, redshift, fid, mjd, mag)
+        self.fid: list[int] = detections["fid"]
+        self.mjd: list[float] = detections["mjd"]
+        self.mag: list[float] = detections["magpsf"]
 
     @property
     def extinction(self):
@@ -30,4 +26,22 @@ class Target:
             radius=0.1,
             catalog="dr5"
         )
+
+def targets_from_TNS_csv(filename):
+    readingtable=pd.read_csv(filename)
+    groups= readingtable["Disc. Internal Name"]
+    groups_nonan= groups.dropna()
+    listofZTFs= []
+    listofRedshifts= readingtable["Redshift"].tolist()
+    listofRAs= readingtable["RA"].tolist()
+    listofDecs= readingtable["DEC"].tolist()
+    for i,row in enumerate(groups_nonan.tolist()):
+        if "ZTF" in row:
+            listofZTFs.append(row)
     
+    targets = []
+    for oid, redshift, ra, dec in zip(listofZTFs, listofRedshifts, listofRAs, listofDecs):
+        target = Target(oid, redshift, ra, dec)
+        targets.append(target)
+    
+    return targets
