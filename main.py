f0555657032dc0fc788ff2954ba069223a52bde0
zknutson <zach.knutson@cfa.harvard.edu>
2026-03-03 16:55:18 -0500

    added a part of the dust extinction code, right now returns a table but need it to instead return the right band for each observation

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from td-data-project!")
-
-
-if __name__ == "__main__":
-    main()
--- target.py
+++ target.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from turtle import pd
-import alerce
+import pandas as pd
+from alerce.core import Alerce
 from pandas import DataFrame
 from astroquery.ipac.irsa.irsa_dust import IrsaDust
 
@@ -9,11 +9,20 @@ class Target:
     def __init__(self, oid, redshift, ra, dec):
         self.oid: str = oid
         self.redshift: float = redshift
-        self.coordinates: tuple[float, float] = (ra, dec)
-        observations = alerce.query_lightcurve(
-            oid=oid,
-            format="pandas"
-        )
+        self.coordinates: tuple[str, str] = (ra, dec)
+        alerce = Alerce()
+        print(f"Querying lightcurve for {oid}...")
+        try:
+            observations = alerce.query_lightcurve(
+                oid=oid,
+                format="pandas"
+            )
+        except Exception as e:
+            print(f"Error querying lightcurve for {oid}: {e}")
+            self.fid = []
+            self.mjd = []
+            self.mag = []
+            return
         detections = DataFrame(observations["detections"][0])
         self.fid: list[int] = detections["fid"]
         self.mjd: list[float] = detections["mjd"]
@@ -21,13 +30,13 @@ class Target:
 
     @property
     def extinction(self):
-        result = IrsaDust.get_query_table(
-            self.coordinates,
-            radius=0.1,
-            catalog="dr5"
-        )
+        import astropy.coordinates as coord
+        import astropy.units as u
+        coords = coord.SkyCoord(self.coordinates[0], self.coordinates[1], frame='fk4',  unit=(u.hourangle, u.deg))
+        table = IrsaDust.get_extinction_table(coords)  
+        return table
 
-def targets_from_TNS_csv(filename):
+def targets_from_TNS_csv(filename, stop_early=0):
     readingtable=pd.read_csv(filename)
     groups= readingtable["Disc. Internal Name"]
     groups_nonan= groups.dropna()
@@ -35,10 +44,14 @@ def targets_from_TNS_csv(filename):
     listofRedshifts= readingtable["Redshift"].tolist()
     listofRAs= readingtable["RA"].tolist()
     listofDecs= readingtable["DEC"].tolist()
+
     for i,row in enumerate(groups_nonan.tolist()):
         if "ZTF" in row:
             listofZTFs.append(row)
     
+    if stop_early:
+        listofZTFs = listofZTFs[:stop_early]
+
     targets = []
     for oid, redshift, ra, dec in zip(listofZTFs, listofRedshifts, listofRAs, listofDecs):
         target = Target(oid, redshift, ra, dec)
