2d4a35e533a6a742bc1dca1dfa9cc1da30c73428
zknutson <zach.knutson@cfa.harvard.edu>
2026-03-10 14:45:24 -0400

    reformatted the api to hopefully be more helpful, and also account for dust extinction and distance modulus (looking for obs_mag column now)

--- target.py
+++ target.py
@@ -5,14 +5,24 @@ import pandas as pd
 from alerce.core import Alerce
 from pandas import DataFrame
 from astroquery.ipac.irsa.irsa_dust import IrsaDust
+from astropy.cosmology import Planck18
+import numpy as np
+import astropy.units as u
 
 Observation = NamedTuple('Observation', [
-    ('redshift', float),
     ('fid', int),
     ('mjd', float),
     ('mag', float),
+    ('abs_mag', float),
+    ('obs_mag', float),
+    ('obs_abs_mag', float),
     ("A_SFD", object),
 ])
+
+def distance_modulus(z):
+    d_L = Planck18.luminosity_distance(z).to(u.Mpc).value
+    mu  = 5.0 * np.log10(d_L) + 25.0
+    return mu, d_L
 
 @dataclass
 class Target:
@@ -36,7 +46,11 @@ class Target:
         detections = DataFrame(observations["detections"][0])
         self.fid: list[int] = detections["fid"]
         self.mjd: list[float] = detections["mjd"]
-        self.mag: list[float] = detections["magpsf"]
+        self.obs_mag: list[float] = detections["magpsf"]
+        dist_mod = distance_modulus(redshift)
+        self.obs_abs_mag: list[float] = [obs_mag - dist_mod[0] for obs_mag in self.obs_mag]
+        self.mag = [obs_mag - ext for obs_mag, ext in zip(self.obs_mag, self.extinction)]
+        self.abs_mag = [obs_abs_mag - ext for obs_abs_mag, ext in zip(self.obs_abs_mag, self.extinction)]
 
     @cached_property
     def extinction(self):
@@ -56,14 +70,63 @@ class Target:
             extinction_values.append(entry[0]["A_SFD"])
         return extinction_values
     
+    def grab_slice_df(self, idxs):
+        return DataFrame({
+            "fid": [self.fid[i] for i in idxs],
+            "mjd": [self.mjd[i] for i in idxs],
+            "obs_mag": [self.obs_mag[i] for i in idxs],
+            "obs_abs_mag": [self.obs_abs_mag[i] for i in idxs],
+            "abs_mag": [self.abs_mag[i] for i in idxs],
+            "mag": [self.mag[i] for i in idxs],
+            "A_SFD": [self.extinction[i] for i in idxs]
+        })
+    
     def __getitem__(self, idx):
-        return Observation(
-            self.redshift,
-            self.fid[idx],
-            self.mjd[idx],
-            self.mag[idx],
-            self.extinction[idx]
-        )
+        if type(idx) == int:
+            return self.grab_slice_df([idx])
+
+        if type(idx) == slice:
+            start, stop, step = idx.indices(len(self.fid))
+            idxs = list(range(start, stop, step))
+            return self.grab_slice_df(idxs)
+        
+        if type(idx) == list or type(idx) == np.ndarray:
+            return self.grab_slice_df(idx)
+        
+        if type(idx) == str:
+            if idx == "g":
+                return self.g_df
+            elif idx == "r":
+                return self.r_df
+            elif idx == "i":
+                return self.i_df
+
+    @cached_property
+    def g_df(self):
+        idxs = [i for i, fid in enumerate(self.fid) if fid == 1]
+        return self.grab_slice_df(idxs)
+    
+    @cached_property
+    def r_df(self):
+        idxs = [i for i, fid in enumerate(self.fid) if fid == 2]
+        return self.grab_slice_df(idxs)
+
+    @cached_property
+    def i_df(self):
+        idxs = [i for i, fid in enumerate(self.fid) if fid == 3]
+        return self.grab_slice_df(idxs)
+    
+    def gr_df(self, tolerance: float = 2):
+        for g_mjd in self.g_df["mjd"]:
+            nearest_delta_mjd = min(abs(r_mjd - g_mjd) for r_mjd in self.r_df["mjd"])
+            if nearest_delta_mjd > tolerance:
+                continue
+            
+
+        print(gr_df)
+        gr_df["delta_mjd"] = abs(gr_df["mjd_g"] - gr_df["mjd_r"])
+        gr_df = gr_df[gr_df["delta_mjd"] <= tolerance]
+        return gr_df
 
 def targets_from_TNS_csv(filename, stop_early=0):
     readingtable=pd.read_csv(filename)
